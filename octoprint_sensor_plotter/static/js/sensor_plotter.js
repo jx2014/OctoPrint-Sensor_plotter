4bb5625b2ce0a09d86944c411a6cc4ed0d9b2dd9
user <user@localhost.localdomain>
2020-11-27 16:15:52 -0800

    template created

---
+++ octoprint_sensor_plotter/static/js/sensor_plotter.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Sensor_plotter
+ *
+ * Author: JX
+ * License: AGPLv3
+ */
+$(function() {
+    function Sensor_plotterViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Sensor_plotterViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_sensor_plotter, #tab_plugin_sensor_plotter, ...
+        elements: [ /* ... */ ]
+    });
+});
