6979547cbecbfd66051491684194c5d80797172c
user <user@localhost.localdomain>
2020-11-29 14:09:02 -0800

    complete OctoPrint plugin tutorials

--- octoprint_sensor_plotter/__init__.py
+++ octoprint_sensor_plotter/__init__.py
@@ -1,79 +1,37 @@
-# coding=utf-8
-from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 
-class Sensor_plotterPlugin(octoprint.plugin.SettingsPlugin,
-                           octoprint.plugin.AssetPlugin,
-                           octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
+class SensorPlotterPlugin(octoprint.plugin.StartupPlugin,
+                          octoprint.plugin.TemplatePlugin,
+                          octoprint.plugin.SettingsPlugin,
+                          octoprint.plugin.AssetPlugin):
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    def on_after_startup(self):
+        self._logger.info("Sensor Plotter (more: %s)" % self._settings.get(["url"]))
 
-	##~~ AssetPlugin mixin
+    def get_settings_defaults(self):
+        return dict(url="https://google.com")
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/sensor_plotter.js"],
-			css=["css/sensor_plotter.css"],
-			less=["less/sensor_plotter.less"]
-		)
+    #def get_template_vars(self):
+    #    return dict(url=self._settings.get(["url"]))
 
-	##~~ Softwareupdate hook
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			sensor_plotter=dict(
-				displayName="Sensor_plotter Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="jx2014",
-				repo="OctoPrint-Sensor_plotter",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/jx2014/OctoPrint-Sensor_plotter/archive/{target_version}.zip"
-			)
-		)
+    def get_assets(self):
+        return dict(
+            js=["js/sensor_plotter.js"],
+            css=["css/sensor_plotter.css"],
+            less=["less/sensor_plotter.less"]
+        )
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Sensor_plotter Plugin"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
-
-def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = Sensor_plotterPlugin()
-
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+__plugin_name__ = "Sensor Plotter"
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = SensorPlotterPlugin()
--- octoprint_sensor_plotter/static/js/sensor_plotter.js
+++ octoprint_sensor_plotter/static/js/sensor_plotter.js
@@ -4,26 +4,47 @@
  * Author: JX
  * License: AGPLv3
  */
+
 $(function() {
-    function Sensor_plotterViewModel(parameters) {
+    function sensor_plotterViewModel(parameters) {
         var self = this;
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        self.settings = parameters[0];
 
-        // TODO: Implement your plugin's view model here.
+        // this will hold the URL currently displayed by the iframe
+        self.currentUrl = ko.observable();
+
+        // this will hold the URL entered in the text field
+        self.newUrl = ko.observable();
+
+        // this will be called when the user clicks the "Go" button and set the iframe's URL to
+        // the entered URL
+        self.goToUrl = function() {
+            self.currentUrl(self.newUrl());
+        };
+
+        // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its
+        // dependencies have already been initialized. It is especially guaranteed that this method
+        // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
+        // the SettingsViewModel been properly populated.
+        self.onBeforeBinding = function() {
+            self.newUrl(self.settings.settings.plugins.sensor_plotter.url());
+            self.goToUrl();
+        }
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    OCTOPRINT_VIEWMODELS.push({
-        construct: Sensor_plotterViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-        // Elements to bind to, e.g. #settings_plugin_sensor_plotter, #tab_plugin_sensor_plotter, ...
-        elements: [ /* ... */ ]
-    });
+    // This is how our plugin registers itself with the application, by adding some configuration
+    // information to the global variable OCTOPRINT_VIEWMODELS
+    OCTOPRINT_VIEWMODELS.push([
+        // This is the constructor to call for instantiating the plugin
+        sensor_plotterViewModel,
+
+        // This is a list of dependencies to inject into the plugin, the order which you request
+        // here is the order in which the dependencies will be injected into your view model upon
+        // instantiation via the parameters argument
+        ["settingsViewModel"],
+
+        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
+        ["#tab_plugin_sensor_plotter"]
+    ]);
 });
